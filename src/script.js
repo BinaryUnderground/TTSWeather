b0f9a307306d7790bfa4c619fce7f1590f053866
Mohammad Hossain <moh.hossain98@gmail.com>
2020-03-29 14:51:47 -0400

    Weather api added

--- src/script.js
+++ src/script.js
@@ -49,4 +49,40 @@ document.getElementById('start-btn').addEventListener("mouseup", function(e) {
       recognition.stop();
 });
 
+Textbox.on("input", function () {
+    Content = document.getElementsByClassName(this).val();
+});
+
+function weatherBalloon(cityName) {
+    var key = "4583df71a6be113c353d48cc4d720e36";
+    fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        cityName +
+        "&appid=" +
+        key
+    )
+        .then(function (resp) {
+            return resp.json();
+        }) // Convert data to json
+        .then(function (data) {
+            drawWeather(data);
+        })
+        .catch(function () {
+            // catch any errors
+        });
+}
+
+window.onload = function () {
+    weatherBalloon("New York");
+};
+
+function drawWeather(d) {
+    var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
+    var fahrenheit = Math.round((parseFloat(d.main.temp) - 273.15) * 1.8 + 32);
+
+    document.getElementById("description").innerHTML = d.weather[0].description;
+    document.getElementById("temp").innerHTML = fahrenheit + "&deg;";
+    document.getElementById("location").innerHTML = d.name;
+}
+
 
