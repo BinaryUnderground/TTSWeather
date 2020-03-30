8445fb3894f75761c5f1c2bb9eb86e0d9ee77013
Oljas Alishev <Oljasalishev@gmail.com>
2020-03-29 20:41:31 -0400

    Made it pretty

--- src/script.js
+++ src/script.js
@@ -85,7 +85,6 @@ window.onload = function () {
 function drawWeather(d) {
     var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
     var fahrenheit = Math.round((parseFloat(d.main.temp) - 273.15) * 1.8 + 32);
-
     document.getElementById("description").innerHTML = d.weather[0].description;
     document.getElementById("temp").innerHTML = fahrenheit + "&deg;";
     document.getElementById("location").innerHTML = d.name;
